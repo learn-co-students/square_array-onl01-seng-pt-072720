84f7adf8885481faf730cb2a2eb957d214ffa45d
daniel campo <campo_daniel94@yahoo.com>
2020-08-04 03:03:45 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,19 @@
+
 def square_array(array)
-  # your code here
+   new_array = []
+    array.each do |numbers|
+     new_array.push(numbers ** 2)
+   end
+   return new_array
 end
+
+
+
+# def square_array(array)
+#   new_array = []
+#     array.each do |numbers| 
+#       squared_number = numbers ** 2
+#       # add this newly squared number to the new_array
+#     end
+#   return new_array
+# end
