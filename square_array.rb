5ea63f6251cf302130073029467430712395dafe
fentyhall <fentymhall@gmail.com>
2020-08-03 19:50:55 -0700

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,13 @@
+# def square_array(array)
+#   new_array = []
+#   array.each do |n| 
+#     new_array << n * n 
+#   end 
+#   new_array 
+# end
+
 def square_array(array)
   new_array = []
-  array.each do |n| 
-    new_array << n * n 
-  end 
-  new_array 
+  array.each { |n| new_array << n*n }
+  new_array
 end
